fff0f0c176044a5d8dc2874682803bb3e0e43eb3
harshivg <ganwaniharshiv@gmail.com>
2024-06-11 11:11:22 +0530

    harshivs first commit

--- frontend/Graph-visualizer/src/App.jsx
+++ frontend/Graph-visualizer/src/App.jsx
@@ -3,7 +3,7 @@ function App() {
     <>
     <div className="bg-slate-400">
       <h2 > Hii everyone</h2>
-      <h1> aakakaak</h1>
+      <h1 className="bg-red-300"> aakakaak</h1>
     </div>
     </>
   )
