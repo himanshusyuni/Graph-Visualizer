8407bd96eb2464a98cb18bea2bd560b655a38a29
Harsh Tayal <harshtayal2005@gmail.com>
2024-06-12 00:03:33 +0530

    Welcome page added

--- frontend/Graph-visualizer/src/App.jsx
+++ frontend/Graph-visualizer/src/App.jsx
@@ -1,9 +1,10 @@
+import Welcome from './components/Welcome'
+
 function App() {
   return (
     <>
     <div className="bg-slate-400">
-      <h2 > Hii everyone</h2>
-      <h1> aakakaak</h1>
+      <Welcome></Welcome>
     </div>
     </>
   )
--- frontend/Graph-visualizer/tailwind.config.js
+++ frontend/Graph-visualizer/tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "dancing-script": ["Dancing Script", "cursive"],
+      },
+      fontWeight: {
+        400: "400",
+        500: "500",
+        600: "600",
+        700: "700",
+      },
+    },
   },
   plugins: [],
-}
+};
