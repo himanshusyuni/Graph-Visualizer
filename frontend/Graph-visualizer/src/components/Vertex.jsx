3fb164888c55d1c4219c674e2e165af37c870421
himanshusyuni <himanshusyuni123@gmail.com>
2024-06-17 21:09:50 +0530

    created graph using array (automactic)

--- frontend/Graph-visualizer/src/components/Vertex.jsx
+++ frontend/Graph-visualizer/src/components/Vertex.jsx
@@ -1,9 +1,17 @@
-function Vertex({ number }) {
-    return (
-        <div className={`p-4 rounded-full bg-purple-600 text-3xl font-semibold  text-white h-16 w-16 flex items-center justify-center hover:bg-rose-500`}>
-            {number}
-        </div>
-    );
+function Vertex({ no, x, y }) {
+  const style = {
+    top: `${y}px`,
+    left: `${x}px`,
+  };
+
+  return (
+    <div
+      style={style}
+      className="p-3 rounded-full bg-purple-600 text-3xl font-semibold text-white h-16 w-16 text-center  z-50 hover:bg-rose-500 absolute "
+    >
+      {no}
+    </div>
+  );
 }
 
 export default Vertex;
