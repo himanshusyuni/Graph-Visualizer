3656c7f6bbbb13f07e5c16e61c2d4561aa072db3
himanshusyuni <himanshusyuni123@gmail.com>
2024-06-20 20:10:54 +0530

    added weighted graph

--- frontend/Graph-visualizer/src/components/LabelledLine.jsx
+++ frontend/Graph-visualizer/src/components/LabelledLine.jsx
@@ -5,21 +5,22 @@ const LabeledLine = ({ x1, y1, x2, y2, color, label }) => {
   const textY = (y1 + y2) / 2;
 
   return (
-    <>
     <div className="absolute z-30">
-
-      <svg  height={450} width={500}>
-        <line x1={x1} y1={y1} x2={x2} y2={y2} stroke={color} strokeWidth="5"  />
+      <svg height={450} width={500}>
+        <line x1={x1} y1={y1} x2={x2} y2={y2} stroke={color} strokeWidth="5" />
         <text
           x={textX}
           y={textY}
           textAnchor="middle"
-          >
+          dominantBaseline="middle"
+          fill="white"
+          fontWeight="600"
+          style={{ zIndex: 200 }} 
+        >
           {label}
         </text>
       </svg>
-            </div>
-    </>
+    </div>
   );
 };
 
--- frontend/Graph-visualizer/src/components/Vertex.jsx
+++ frontend/Graph-visualizer/src/components/Vertex.jsx
@@ -7,7 +7,7 @@ function Vertex({ no, x, y }) {
   return (
     <div
       style={style}
-      className="p-3 rounded-full bg-blue-900 text-3xl font-semibold text-white h-16 w-16 text-center  z-50 hover:bg-rose-500 absolute "
+      className="p-3 rounded-full bg-blue-600 text-3xl font-semibold text-white h-16 w-16 text-center  z-50 hover:bg-rose-500 absolute "
     >
       {no}
     </div>
