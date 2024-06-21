150d67ad64be4eed5f384a56738e297e3894f377
himanshu <himanshusingh2087@gmail.com>
2024-06-22 02:38:36 +0530

    styling added

--- frontend/Graph-visualizer/src/components/UserInput.jsx
+++ frontend/Graph-visualizer/src/components/UserInput.jsx
@@ -57,19 +57,24 @@ function UserInput() {
 
   return (
     <>
-      <div className="bg-[#fbfae5] pb-10 min-h-screen flex flex-col items-center lg:items-start">
-        <div className="font-bold text-center text-[#00d0aa]  bg-[#0b2834] text-[2rem] font-serif underline mb-5 p-5 italic w-full">
-          Graph Visualizer
+    <div className=" bg-[#fbfae5] h-full w-full flex flex-col shadow-lg"  >
+    <div className="font-bold text-center   underline text-rose-900 mb-8  flex flex col justify-center items-center text-teal-400 bg-[#0b2834] font-serif   italic h-full w-full  text-[2rem]">
+    Graph Visualizer     <img className="h-11 w-16 mx-1 "
+          src="pic1.svg" alt="" /> 
+          
         </div>
-        <div className="flex flex-col items-center xlg:flex-row px-10 w-full min-h-screen gap-10 xlg:gap-10 xlg:justify-between xlg:items-start">
+        
+   
+       
+        <div className="flex flex-col items-center xlg:flex-row px-10 w-full min-h-screen gap-10 xlg:gap-10 xlg:justify-between xlg:items-start shadow-lg">
           <div className="w-[30rem] md:w-[45rem] xlg:w-[40rem]  flex flex-col gap-5 h-[30rem] md:h-[40rem] rounded-3xl border-[0.3rem]  border-teal-950 overflow-auto">
             <div className="bg-[#fbfae5] p-2 h-[40rem]">
               <div className="flex justify-center mt-4">
-                <div className="bg-gradient-to-r from-teal-900 to-teal-600 flex gap-3 px-3 py-2 rounded-full border-2 border-black text-[0.8rem] md:text-[1.1rem] text-white font-bold">
+                <div className="bg-gradient-to-r from-teal-900 to-teal-600 flex gap-3 px-3 py-2 rounded-full border-2 border-black text-[0.8rem] md:text-[1.1rem] text-white font-bold shadow-lg">
                   <div
                     className={
                       (toggle === 0 &&
-                        `bg-gradient-to-r from-emerald-400 to-teal-600 py-2 px-10 rounded-3xl hover:cursor-pointer text-black`) ||
+                        `bg-gradient-to-r from-emerald-400 to-teal-950 py-2 px-10 rounded-3xl hover:cursor-pointer text-black`) ||
                       `py-2 px-10 rounded-3xl hover:cursor-pointer`
                     }
                     onClick={() => handleToggle()}
@@ -91,24 +96,24 @@ function UserInput() {
               {toggle == 0 && (
                 <>
                   <div className="flex pt-10 pl-2 ">
-                    <p className="pr-5 text-[1.2rem] md:text-[1.5rem] text-[#2a3b68] bg-clip-text text-transparent  font-semibold">
+                    <p className="pr-5 text-[1.2rem] md:text-[1.5rem] text-[#2a3b68] bg-clip-text   font-semibold shadow-lg">
                       Number of Vertex (Max upto 10) :
                     </p>
                     <input
                       type="number"
                       value={Vertex}
-                      className="w-[5rem] text-[1.2rem] border-2 border-teal-700 bg-gradient-to-r from-teal-400 to-yellow-200 focus:border-0 pl-2 rounded-md"
+                      className="w-[5rem] text-[1.2rem] border-2 border-teal-700 bg-gradient-to-r from-emerald-400 to-teal-950 focus:border-0 pl-2 rounded-md"
                       onChange={(e) => handleVertex(e)}
                     ></input>
                   </div>
                   <div className="flex pt-8 pl-2">
-                    <p className="pr-5 text-[1.2rem] md:text-[1.5rem] text-[#2a3b68] bg-clip-text text-transparent  font-semibold">
+                    <p className="pr-5 text-[1.2rem] md:text-[1.5rem] text-[#2a3b68] bg-clip-text   font-semibold shadow-lg">
                       Type of Graph :{" "}
                     </p>
                     <div class="flex border-[0.15rem] border-teal-700 bg-[#fbfae5] rounded-md overflow-hidden">
                       <button
                         className={` text-black font-bold p-2 border-r-2 border-black  active:bg-gradient-to-r from-emerald-400 to-teal-600 ${
-                          Type == 0 && "bg-gradient-to-r from-emerald-400 to-teal-600"
+                          Type == 0 && "bg-gradient-to-r from-emerald-400 to-teal-950"
                         } `}
                         onClick={() => setType(0)}
                       >
@@ -116,7 +121,7 @@ function UserInput() {
                       </button>
                       <button
                         className={` text-black font-bold p-2  active:bg-gradient-to-r from-emerald-400 to-teal-600 ${
-                          Type == 1 && "bg-gradient-to-r from-emerald-400 to-teal-600"
+                          Type == 1 && "bg-gradient-to-r from-emerald-400 to-teal-950"
                         }  `}
                         onClick={() => setType(1)}
                       >
@@ -124,12 +129,12 @@ function UserInput() {
                       </button>
                     </div>
                   </div>
-                  <div className="flex pl-2 pt-8">
-                    <p className="pr-5 text-[1.2rem] md:text-[1.5rem] text-[#2a3b68]  font-semibold">
+                  <div className="flex pl-2 pt-8 ">
+                    <p className="pr-5 text-[1.2rem] md:text-[1.5rem] text-[#2a3b68]  font-semibold shadow-lg mb-24 mt-1 h-10 w-32">
                       Edge List:{" "}
                     </p>
                     <textarea
-                      className="resize-none rounded-2xl text-lg p-2 bg-gradient-to-r from-teal-400 to-yellow-200 border-[0.18rem] border-teal-700 outline-none w-[60%] placeholder-gray-500"
+                      className="resize-none rounded-2xl text-lg p-2 bg-gradient-to-r from-emerald-400 to-teal-950  border-[0.18rem] border-teal-700 outline-none w-[60%] placeholder-gray-500"
                       rows={5}
                       cols={30}
                       ref={edgeList}
@@ -140,7 +145,7 @@ function UserInput() {
 
                   <div className="flex justify-between m-5 mt-20">
                     <button
-                      className="bg-[#0b2834] text-white font-bold py-2 text-2xl px-8 rounded-2xl border-2 border-teal-950"
+                      className="bg-[#0b2834] text-white font-bold py-2 text-2xl px-8 rounded-2xl border-2 border-teal-950 shadow-black"
                       onClick={() => navigate("/")}
                     >
                       Back
@@ -158,7 +163,7 @@ function UserInput() {
             </div>
           </div>
 
-          <div className="min-h-screen ">
+          <div className="min-h-screen shadow-lg rounded-3xl overflow-hidden mb-2">
             <div className=" border-4 border-black bg-gradient-to-r from-teal-900 to-teal-600 h-[30rem] md:h-[40rem] xlg:w-[40rem] md:w-[45rem] w-[30rem] rounded-3xl ">
               {CompToShow === "Demo" && <DemoGraph />}
               {CompToShow == "Process" && <ProcessingPage />}
